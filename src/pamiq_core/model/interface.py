33e6b9c095615a64b96349aa2096950d222d6069
zassou65535 <gravity@kuf.biglobe.ne.jp>
2025-02-15 12:56:01 +0000

    add docstrings

--- src/pamiq_core/model/interface.py
+++ src/pamiq_core/model/interface.py
@@ -3,16 +3,58 @@ from typing import Any
 
 
 class InferenceModel(ABC):
+    """Wrapper class for model to infer.
+
+    Needed for multi-thread training and inference in parallel.
+    """
+
     @abstractmethod
     def infer(self, *args, **kwds) -> Any:
+        """Perform inference using a model.
+
+        Args:
+            *args:
+                Positional arguments required for inference.
+            **kwds:
+                Keyword arguments required for inference.
+        Returns:
+            Any:
+                The result of the inference.
+        """
         raise NotImplementedError
 
     def __call__(self, *args, **kwds) -> Any:
+        """Perform inference using a model.
+
+        Args:
+            *args:
+                Positional arguments required for inference.
+            **kwds:
+                Keyword arguments required for inference.
+        Returns:
+            Any:
+                The result of the inference.
+        """
         return self.infer(*args, **kwds)
 
 
 class TrainingModel(ABC):
+    """Wrapper class to train model.
+
+    Needed for multi-thread training and inference in parallel.
+    """
+
     def __init__(self, has_inference: bool = True, inference_only: bool = False):
+        """Initialize.
+
+        Args:
+            has_inference (bool):
+                Whether to have InferenceModel.
+                Default to True.
+            inference_only (bool):
+                Whether to do Inference only.
+                Default to False.
+        """
         if inference_only and not has_inference:
             raise ValueError
         self.has_inference = has_inference
@@ -22,6 +64,11 @@ class TrainingModel(ABC):
 
     @property
     def inference_model(self) -> InferenceModel:
+        """Get inference model.
+
+        Returns:
+            InferenceModel: A model to infer.
+        """
         if not self.has_inference:
             raise RuntimeError
 
@@ -31,11 +78,38 @@ class TrainingModel(ABC):
 
     @abstractmethod
     def _create_inference_model(self) -> InferenceModel:
+        """Create inference model.
+
+        Returns:
+            InferenceModel: A model to infer.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def forward(self, *args, **kwds) -> Any:
+        """Running model for training.
+
+        Args:
+            *args:
+                Positional arguments required for training.
+            **kwds:
+                Keyword arguments required for training.
+        Returns:
+            Any:
+                Result of running the model.
+        """
         raise NotImplementedError
 
     def __call__(self, *args, **kwds) -> Any:
+        """Running model for training.
+
+        Args:
+            *args:
+                Positional arguments required for training.
+            **kwds:
+                Keyword arguments required for training.
+        Returns:
+            Any:
+                Result of running the model.
+        """
         return self.forward(*args, **kwds)
