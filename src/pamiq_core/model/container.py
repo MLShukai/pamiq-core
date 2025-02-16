a763438a10d28c3b7ca0c095537b8456253d9603
zassou65535 <gravity@kuf.biglobe.ne.jp>
2025-02-16 10:08:32 +0000

    add docstrings

--- src/pamiq_core/model/container.py
+++ src/pamiq_core/model/container.py
@@ -3,19 +3,30 @@ from collections import UserDict
 from .interface import InferenceModel, TrainingModel
 
 
-class InferenceModelsDict(UserDict[str, InferenceModel]): ...
+class InferenceModelsDict(UserDict[str, InferenceModel]):
+    """Wrapper class for models to infer."""
+
+    ...
 
 
 class TrainingModelsDict(UserDict[str, TrainingModel]):
+    """Wrapper class to train model."""
+
     _inference_models_dict: InferenceModelsDict | None = None
 
     @property
     def inference_models_dict(self) -> InferenceModelsDict:
+        """Make InferenceModelsDict."""
         if self._inference_models_dict is None:
             self._inference_models_dict = self._create_inferences()
         return self._inference_models_dict
 
     def _create_inferences(self) -> InferenceModelsDict:
+        """Select training models that has inference_model and isn't inference
+        only and return them as InferenceModelsDict.
+
+        Returns: InferenceModelsDict
+        """
         return InferenceModelsDict(
             {
                 model_name: training_model.inference_model
