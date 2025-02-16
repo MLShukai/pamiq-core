f044035a944f38de856005913e520e7eff91ccb4
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2025-02-16 01:22:29 +0000

    feat: add exports for DataBuffer, DataCollector, and related classes

---
+++ src/pamiq_core/data/__init__.py
@@ -0,0 +1,13 @@
+from .buffer import BufferData, DataBuffer, StepData
+from .container import DataCollectorsDict, DataUsersDict
+from .interface import DataCollector, DataUser
+
+__all__ = [
+    "DataBuffer",
+    "StepData",
+    "BufferData",
+    "DataCollector",
+    "DataUser",
+    "DataCollectorsDict",
+    "DataUsersDict",
+]
