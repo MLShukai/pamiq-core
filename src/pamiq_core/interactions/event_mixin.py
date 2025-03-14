60dec930340f9aefe349cdb1bba47f3c0a31255d
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-03-14 05:54:21 +0000

    fix(interaction): previous module dir and move test dir

--- src/pamiq_core/interactions/event_mixin.py
+++
@@ -1,8 +0,0 @@
-class InteractionEventMixin:
-    """Define event functions for interactions."""
-
-    def setup(self) -> None:
-        """It is called first when an Interaction is performed."""
-
-    def teardown(self) -> None:
-        """It is called last when performing an Interaction."""
