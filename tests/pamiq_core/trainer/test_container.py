c4a05a15a7c8b120e7139d1e0ac90b409e782c6c
zassou65535 <gravity@kuf.biglobe.ne.jp>
2025-03-16 09:07:03 +0000

    add tests

--- tests/pamiq_core/trainer/test_container.py
+++ tests/pamiq_core/trainer/test_container.py
@@ -40,35 +40,42 @@ class TestTrainersDict:
         return DataUsersDict({"data": mock_user})
 
     @pytest.fixture
-    def trainer(self) -> Trainer:
-        return DummyTrainer()
-
-    @pytest.fixture
-    def trainer_attached(
+    def trainer(
         self,
-        trainer: Trainer,
         training_models_dict: TrainingModelsDict,
         data_users_dict: DataUsersDict,
     ) -> Trainer:
+        trainer = DummyTrainer()
         trainer.attach_training_models_dict(training_models_dict=training_models_dict)
         trainer.attach_data_users_dict(data_users_dict=data_users_dict)
         return trainer
 
     @pytest.fixture
-    def trainers_dict(self) -> TrainersDict:
-        return TrainersDict()
-
-    @pytest.fixture
-    def trainers_dict_attached(self, trainer_attached: Trainer) -> TrainersDict:
-        return TrainersDict({"trainer": trainer_attached})
+    def trainers_dict(self, trainer: Trainer) -> TrainersDict:
+        return TrainersDict({"trainer": trainer})
 
     def test_get_trainers_list(
-        self, trainers_dict_attached: TrainersDict, trainer_attached: Trainer
+        self, trainers_dict: TrainersDict, trainer: Trainer
     ) -> None:
-        assert trainers_dict_attached.get_trainers_list() == [trainer_attached]
+        assert trainers_dict.get_trainers_list() == [trainer]
 
-    # def test_get_trainable_trainer(self) -> None:
+    def test_get_trainable_trainer(
+        self, trainers_dict: TrainersDict, trainer: Trainer
+    ) -> None:
+        assert trainers_dict.get_trainable_trainer() is trainer
 
-    # def test_attach_training_models_dict(self, training_models_dict: TrainingModelsDict) -> None:
+    def test_attach_training_models_dict(
+        self, mocker: MockerFixture, trainers_dict: TrainersDict
+    ) -> None:
+        new_training_models_dict = mocker.Mock(TrainingModelsDict)
+        trainers_dict.attach_training_models_dict(new_training_models_dict)
+        assert (
+            trainers_dict["trainer"]._training_models_dict is new_training_models_dict
+        )
 
-    # def test_attach_data_users_dict(self, data_users_dict: DataUsersDict) -> None:
+    def test_attach_data_users_dict(
+        self, mocker: MockerFixture, trainers_dict: TrainersDict
+    ) -> None:
+        new_data_users_dict = mocker.Mock(DataUsersDict)
+        trainers_dict.attach_data_users_dict(new_data_users_dict)
+        assert trainers_dict["trainer"]._data_users_dict is new_data_users_dict
