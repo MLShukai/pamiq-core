9102e86c6597a9a91f407d5b57ac986de7b6b267
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2025-03-22 00:46:35 +0000

    feat(interaction): expose Interaction class to module top level

--- src/pamiq_core/interaction/__init__.py
+++ src/pamiq_core/interaction/__init__.py
@@ -1,10 +1,12 @@
 from . import modular_env, wrappers
 from .agent import Agent
 from .env import Environment
+from .interactions import Interaction
 
 __all__ = [
     "Agent",
     "Environment",
     "modular_env",
     "wrappers",
+    "Interaction",
 ]
