701d78ffd5d70abc155e58e81ae7baadf2e93697
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2025-04-03 11:54:52 +0900

    AWSのGPU Runnerを追加 (#134)
    * feat(workflow): add gpu test workflow file
    
    * chore: rename gpu runner
    
    * feat(workflow): add gpu test workflow
    
    * fix: tor typo
    
    * chore: suppress logs and remove cov for targetting test
    
    * fix(ci): remove uv depedency cache
    
    * chore(ci): rename test
    
    * feat(ci): check the nvidia driver
    
    * debug(ui): lspci
    
    * fix(ci): install pciutils
    
    * fix(ci): fix installation command
    
    * fix(ci): ensure nvidia driver version
    
    * fix(ci): ensure pytorch cuda available
    
    * debug(ci): cuda diagnostics
    
    * chore(torch): log available devices
    
    * chore: remove timeout minutes
    
    * chore(ci): check os release
    
    * chore(ci): install cuda
    
    * remove cuda install and run nvidia-smi
    
    * fix installation command
    
    * fix installation
    
    * fix driver installation
    
    * modprobe nvidia
    
    * remove nvidia driver installation
    
    * fix(ci): restrict test condition
    
    * chore(ci): remove debug message

--- tests/pamiq_core/torch/test_model.py
+++ tests/pamiq_core/torch/test_model.py
@@ -1,6 +1,4 @@
-import copy
-from threading import RLock
-from typing import Any, Protocol, override
+import logging
 
 import pytest
 import torch
@@ -14,6 +12,8 @@ from pamiq_core.torch import (
     get_device,
 )
 
+logger = logging.getLogger(__name__)
+
 CPU_DEVICE = torch.device("cpu")
 CUDA_DEVICE = torch.device("cuda:0")
 
@@ -24,6 +24,8 @@ def get_available_devices() -> list[torch.device]:
         devices.append(CUDA_DEVICE)
     return devices
 
+
+logger.info("Available devices: " + ", ".join(map(str, get_available_devices())))
 
 parametrize_device = pytest.mark.parametrize("device", get_available_devices())
 
