28ad43fa64c5f664d426b36f8e70610eacf3cf7b
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2025-04-03 16:58:52 +0900

    PyTorchをオプショナルな依存関係に変更 (#138)

--- src/pamiq_core/torch/__init__.py
+++ src/pamiq_core/torch/__init__.py
@@ -1,3 +1,13 @@
+try:
+    import torch  # pyright: ignore[reportUnusedImport]
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        "torch module not found. To use torch features of pamiq-core, "
+        "please run the following command:\n\n"
+        "    pip install pamiq-core[torch]\n"
+    )
+
+
 from .model import (
     TorchInferenceModel,
     TorchTrainingModel,
