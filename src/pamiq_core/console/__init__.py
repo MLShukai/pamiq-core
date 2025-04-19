63b1b597aefc5748638e5b81e2c2b4741b3d3825
zassou65535 <gravity@kuf.biglobe.ne.jp>
2025-04-19 14:25:58 +0000

    update exporting

--- src/pamiq_core/console/__init__.py
+++ src/pamiq_core/console/__init__.py
@@ -1,7 +1,9 @@
+from .cui import Console
 from .system_status import SystemStatus, SystemStatusProvider
 from .web_api_handler import ControlCommands, WebApiHandler
 
 __all__ = [
+    "Console",
     "SystemStatus",
     "SystemStatusProvider",
     "WebApiHandler",
