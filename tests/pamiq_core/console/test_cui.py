a0e2b9bb7e6d99bef89d0b8a82c8ceb81350770e
zassou65535 <gravity@kuf.biglobe.ne.jp>
2025-04-22 08:25:45 +0000

    remove cmd.Cmd and introduce prompt_toolkit

--- tests/pamiq_core/console/test_cui.py
+++ tests/pamiq_core/console/test_cui.py
@@ -29,15 +29,15 @@ class TestConsole:
     def console(self, mock_httpx) -> Console:
         return Console(host="localhost", port=8391)
 
-    def test_initial_prompt(self, mock_httpx) -> None:
-        console = Console("localhost", 8391)
-        assert console.prompt == "pamiq-console (running) > "
+    def test_fetch_status_when_online(self, console: Console, mock_httpx) -> None:
+        console.fetch_status()
+        assert console.status == "running"
 
-    def test_initial_prompt_when_offline(self, mock_httpx) -> None:
+    def test_fetch_status_when_offline(self, console: Console, mock_httpx) -> None:
         mock_httpx.RequestError = httpx.RequestError
         mock_httpx.get.side_effect = httpx.RequestError("Test RequestError")
-        console = Console("localhost", 8391)
-        assert console.prompt == "pamiq-console (offline) > "
+        console.fetch_status()
+        assert console.status == "offline"
 
     def test_get_all_commands(self, console: Console) -> None:
         assert set(console.get_all_commands()) == {
@@ -53,7 +53,7 @@ class TestConsole:
             "quit",
         }
 
-    def test_onecmd(
+    def test_run_command(
         self,
         console: Console,
         mock_httpx,
@@ -64,8 +64,8 @@ class TestConsole:
         mock_httpx.get.side_effect = httpx.RequestError("Test RequestError")
         # test each command
         for command_name in console.get_all_commands():
-            console.onecmd(command_name)
-            assert console.prompt == "pamiq-console (offline) > "
+            console.run_command(command_name)
+            assert console.status == "offline"
             captured = capsys.readouterr()
             if command_name in [
                 "pause",
@@ -79,10 +79,10 @@ class TestConsole:
             else:
                 assert f'Command "{command_name}" not executed.' not in captured.out
 
-    def test_help_command(
+    def test_command_help(
         self, console: Console, capsys: pytest.CaptureFixture[str]
     ) -> None:
-        console.do_help("")
+        console.command_help()
         captured = capsys.readouterr()
         # Check if "help" explains all commands.
         captured_commands: list[str] = []
@@ -94,53 +94,55 @@ class TestConsole:
                 captured_commands += cmds
         assert set(console.get_all_commands()) == set(captured_commands)
 
-    def test_do_h_as_alias(
+    def test_command_h_as_alias(
         self, mocker: MockerFixture, console: Console, mock_httpx
     ) -> None:
-        mock_help = mocker.spy(console, "do_help")
-        console.do_h("")
-        mock_help.assert_called_once_with("")
+        mock_help = mocker.spy(console, "command_help")
+        console.command_h()
+        mock_help.assert_called_once_with()
 
-    def test_do_pause(
+    def test_command_pause(
         self,
         console: Console,
         mock_httpx,
         capsys: pytest.CaptureFixture[str],
     ) -> None:
-        mock_httpx.post.return_value.text = json.dumps({"result": "test do_pause"})
-        console.do_pause("")
+        mock_httpx.post.return_value.text = json.dumps({"result": "test command_pause"})
+        console.command_pause()
         mock_httpx.post.assert_called_once_with("http://localhost:8391/api/pause")
         captured = capsys.readouterr()
-        assert "test do_pause" in captured.out
+        assert "test command_pause" in captured.out
 
-    def test_do_p_as_alias(
+    def test_command_p_as_alias(
         self, mocker: MockerFixture, console: Console, mock_httpx
     ) -> None:
-        mock_pause = mocker.spy(console, "do_pause")
-        console.do_p("")
-        mock_pause.assert_called_once_with("")
+        mock_pause = mocker.spy(console, "command_pause")
+        console.command_p()
+        mock_pause.assert_called_once_with()
 
-    def test_do_resume(
+    def test_command_resume(
         self,
         console: Console,
         mock_httpx,
         capsys: pytest.CaptureFixture[str],
     ) -> None:
-        mock_httpx.post.return_value.text = json.dumps({"result": "test do_resume"})
-        console.do_resume("")
+        mock_httpx.post.return_value.text = json.dumps(
+            {"result": "test command_resume"}
+        )
+        console.command_resume()
         mock_httpx.post.assert_called_once_with("http://localhost:8391/api/resume")
         captured = capsys.readouterr()
-        assert "test do_resume" in captured.out
+        assert "test command_resume" in captured.out
 
-    def test_do_r_as_alias(
+    def test_command_r_as_alias(
         self, mocker: MockerFixture, console: Console, mock_httpx
     ) -> None:
-        mock_resume = mocker.spy(console, "do_resume")
-        console.do_r("")
-        mock_resume.assert_called_once_with("")
+        mock_resume = mocker.spy(console, "command_resume")
+        console.command_r()
+        mock_resume.assert_called_once_with()
 
     @pytest.mark.parametrize("users_answer", ["y", "yes"])
-    def test_do_shutdown_yes(
+    def test_command_shutdown_yes(
         self,
         console: Console,
         mock_httpx,
@@ -151,15 +153,17 @@ class TestConsole:
         monkeypatch.setattr(
             "builtins.input", lambda prompt: users_answer
         )  # Mock input() to return <users_answer>.
-        mock_httpx.post.return_value.text = json.dumps({"result": "test do_shutdown"})
-        result = console.do_shutdown("")
+        mock_httpx.post.return_value.text = json.dumps(
+            {"result": "test command_shutdown"}
+        )
+        result = console.command_shutdown()
         mock_httpx.post.assert_called_once_with("http://localhost:8391/api/shutdown")
         captured = capsys.readouterr()
-        assert "test do_shutdown" in captured.out
+        assert "test command_shutdown" in captured.out
         assert result is True
 
     @pytest.mark.parametrize("users_answer", ["n", "N", "other_strings"])
-    def test_do_shutdown_no(
+    def test_command_shutdown_no(
         self,
         console: Console,
         mock_httpx,
@@ -170,41 +174,43 @@ class TestConsole:
         monkeypatch.setattr(
             "builtins.input", lambda prompt: users_answer
         )  # Mock input() to return <users_answer>.
-        mock_httpx.post.return_value.text = json.dumps({"result": "test do_shutdown"})
-        result = console.do_shutdown("")
+        mock_httpx.post.return_value.text = json.dumps(
+            {"result": "test command_shutdown"}
+        )
+        result = console.command_shutdown()
         captured = capsys.readouterr()
         assert "Shutdown cancelled" in captured.out
         assert result is False
 
-    def test_do_quit(self, console: Console) -> None:
-        result = console.do_quit("")
+    def test_command_quit(self, console: Console) -> None:
+        result = console.command_quit()
         assert result is True
 
-    def test_do_q_as_alias(self, mocker: MockerFixture, console: Console) -> None:
-        mock_quit = mocker.spy(console, "do_quit")
-        console.do_q("")
-        mock_quit.assert_called_once_with("")
+    def test_command_q_as_alias(self, mocker: MockerFixture, console: Console) -> None:
+        mock_quit = mocker.spy(console, "command_quit")
+        console.command_q()
+        mock_quit.assert_called_once_with()
 
-    def test_do_save(
+    def test_command_save(
         self,
         console: Console,
         mock_httpx,
         capsys: pytest.CaptureFixture[str],
     ) -> None:
-        mock_httpx.post.return_value.text = json.dumps({"result": "test do_save"})
-        console.do_save("")
+        mock_httpx.post.return_value.text = json.dumps({"result": "test command_save"})
+        console.command_save()
         mock_httpx.post.assert_called_once_with("http://localhost:8391/api/save-state")
         captured = capsys.readouterr()
-        assert "test do_save" in captured.out
+        assert "test command_save" in captured.out
 
-    @pytest.mark.parametrize("exit_console", [True, False])
-    def test_postcmd_updates_status(
-        self, console: Console, mock_httpx, exit_console: bool
-    ) -> None:
-        mock_httpx.get.return_value.text = json.dumps({"status": "test postcmd"})
-        result = console.postcmd(stop=exit_console, line="")
-        assert console.prompt == "pamiq-console (test postcmd) > "
-        assert result is exit_console
+    # @pytest.mark.parametrize("exit_console", [True, False])
+    # def test_postcmd_updates_status(
+    #     self, console: Console, mock_httpx, exit_console: bool
+    # ) -> None:
+    #     mock_httpx.get.return_value.text = json.dumps({"status": "test postcmd"})
+    #     result = console.postcmd(stop=exit_console, line="")
+    #     assert console.prompt == "pamiq-console (test postcmd) > "
+    #     assert result is exit_console
 
 
 def test_main(mocker: MockerFixture) -> None:
@@ -216,4 +222,4 @@ def test_main(mocker: MockerFixture) -> None:
     mock_console_class.return_value = mock_console
     main()
     mock_console_class.assert_called_once_with("test-host.com", 1938)
-    mock_console.cmdloop.assert_called_once_with()
+    mock_console.main_loop.assert_called_once_with()
