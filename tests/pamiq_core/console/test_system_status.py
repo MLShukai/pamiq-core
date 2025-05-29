1b8ccdacb925bca88c5953095f86fbafe1b176c6
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-05-29 23:48:10 +0000

    feat(console): add offline status

--- tests/pamiq_core/console/test_system_status.py
+++ tests/pamiq_core/console/test_system_status.py
@@ -239,6 +239,7 @@ class TestSystemStatusEnum:
         assert SystemStatus.PAUSED.status_name == "paused"
         assert SystemStatus.RESUMING.status_name == "resuming"
         assert SystemStatus.SHUTTING_DOWN.status_name == "shutting down"
+        assert SystemStatus.OFFLINE.status_name == "offline"
 
         for status in SystemStatus:
             status.status_name
